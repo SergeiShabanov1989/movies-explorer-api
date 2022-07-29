3cdb9dfeb9ba3d694ec78900b762a3ab59f45877
Sergei Shabanov <shabanov1989@gmail.com>
2022-07-29 15:01:22 +0300

    feat: добавил rate-limiter и helmet

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const { ERROR, DB_URL } = require('./utils/utils');
 const routes = require('./routes');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { limiter } = require('./middlewares/limiter');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(limiter);
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
