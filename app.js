d9d193e1c3be34ebbbebf163e953632ce48d8aeb
Sergei Shabanov <shabanov1989@gmail.com>
2022-09-16 15:36:13 +0300

    feat: add domain

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter } = require('./middlewares/limiter');
 const { handleErrors } = require('./middlewares/handleErrors');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 const options = {
